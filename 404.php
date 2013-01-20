cebc186e02053d95ff16dc201591c28eebc72527
Lucas Rocha <lucasr@lucasr.org>
2013-01-20 23:32:29 +0000

    [404] Add page for the 404 case

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The 404 template file.
+ *
+ * @package WordPress
+ * @subpackage lucasr
+ */
+
+get_header();?>
+
+      <div class="row-fluid">
+
+        <div class="span12">
+
+          <?php get_template_part( 'content', 'none' ); ?>
+
+        </div> <!-- span12 -->
+
+      </div> <!-- row-fluid -->
+
+<?php get_footer(); ?>
--- content-none.php
+++ content-none.php
@@ -12,7 +12,14 @@
             <header class="entry-header">
 
               <hgroup>
-                <h1 class="entry-title"><?php _e( 'Nothing found', 'lucasr' ) ?></h1>
+                <h1 class="entry-title">
+                  <?php if ( is_404() ) : ?>
+                    <?php _e( 'Oops! Page not found.', 'lucasr' ) ?>
+                  <?php else : ?>
+                    <?php _e( 'Nothing found.', 'lucasr' ) ?>
+                  <?php endif; // end is_404() check ?>
+                </h1>
+
                 <h2 class="entry-date"><?php _e( 'Try a search?', 'lucasr' ); ?></h2>
               </hgroup>
 
